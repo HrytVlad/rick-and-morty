da285ca8184095e8b9260a4bc27dab480bbd4aba
KappRoss <rostyslav.kaplychnyi@underdogfantasy.com>
2023-02-13 12:52:00 +0200

    Added swagger documentation using drf-spectacular

--- characters/views.py
+++ characters/views.py
@@ -1,6 +1,7 @@
 import random
 
 from django.shortcuts import render
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import status, generics
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
@@ -9,7 +10,9 @@ from rest_framework.response import Response
 from characters.models import Character
 from characters.serializers import CharacterSerializer
 
-
+@extend_schema(
+    responses={status.HTTP_200_OK: CharacterSerializer}
+)
 @api_view(["GET"])
 def get_random_character_view(request: Request) -> Response:
     pks = Character.objects.values_list("pk", flat=True)
@@ -28,3 +31,16 @@ class CharacterListView(generics.ListAPIView):
         if name is not None:
             queryset = queryset.filter(name__icontains=name)
         return queryset
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="name",
+                description="Filter by name",
+                required=False,
+                type=str
+            ),
+        ]
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
